36da337a438ff8af4bda93964b103c07ae62540d
hawardShin <joushin@student.42seoul.kr>
2024-07-11 10:37:17 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/techwave/olol/OlolApplication.java
@@ -0,0 +1,13 @@
+package com.techwave.olol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlolApplication.class, args);
+	}
+
+}
