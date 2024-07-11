c80836f63ec5623991bea30da093f9c627986625
hawardShin <joushin@student.42seoul.kr>
2024-07-11 10:37:17 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/techwave/olol/OlolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techwave.olol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
