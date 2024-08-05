8fbe7f1d28676acc688aed36f5e08979e17c230f
hawardShin <joushin@student.42seoul.kr>
2024-08-05 15:38:23 +0900

    Feat: Set Cheer Entity

---
+++ src/main/java/com/techwave/olol/cheer/domain/Cheer.java
@@ -0,0 +1,47 @@
+package com.techwave.olol.cheer.domain;
+
+import com.techwave.olol.mission.domain.Mission;
+import com.techwave.olol.user.domain.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "user_mission_cheer")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cheer {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "type", nullable = false)
+	private CheerType cheerType;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "giver_id")
+	private User cheerGiver;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "mission_id")
+	private Mission mission;
+
+	@Builder
+	public Cheer(CheerType cheerType) {
+		this.cheerType = cheerType;
+	}
+	
+}
