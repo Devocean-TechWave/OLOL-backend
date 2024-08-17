9f95d3977bdf036528403ce387d39513f209e1e4
hawardShin <joushin@student.42seoul.kr>
2024-08-17 09:26:27 +0900

    Chore: CheckStyle

--- src/main/java/com/techwave/olol/mission/dto/request/ReqMissionDto.java
+++ src/main/java/com/techwave/olol/mission/dto/request/ReqMissionDto.java
@@ -1,13 +1,11 @@
 package com.techwave.olol.mission.dto.request;
 
-import com.techwave.olol.user.domain.User;
+import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 @Builder
 @AllArgsConstructor
