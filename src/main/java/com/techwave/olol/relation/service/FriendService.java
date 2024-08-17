a1b1a75793d91127f0eee6ae2f86087e7743d36e
hawardShin <joushin@student.42seoul.kr>
2024-08-17 11:12:23 +0900

    Feat: 친구 요청 보내기

--- src/main/java/com/techwave/olol/relation/service/FriendService.java
+++ src/main/java/com/techwave/olol/relation/service/FriendService.java
@@ -3,18 +3,25 @@ package com.techwave.olol.relation.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.techwave.olol.relation.domain.RelationType;
 import com.techwave.olol.relation.domain.UserRelationShip;
 import com.techwave.olol.relation.dto.response.FriendReqListDto;
 import com.techwave.olol.relation.repository.UserRelationShipRepository;
+import com.techwave.olol.user.domain.User;
+import com.techwave.olol.user.dto.UserInfoDto;
+import com.techwave.olol.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class FriendService {
 
 	private final UserRelationShipRepository userRelationShipRepository;
+	private final UserRepository userRepository;
 
 	/**
 	 * 내가 보낸 친구 요청 목록을 가져온다.
@@ -54,9 +61,24 @@ public class FriendService {
 		return friendReqListDto;
 	}
 
-	// public UserInfoDto requestFriend(String userId) {
-	// 	return new UserInfoDto();
-	// }
+	@Transactional
+	public UserInfoDto requestFriend(String senderId, String receiverId, RelationType relationType) {
+		User sender = userRepository.findUserById(senderId);
+		User receiver = userRepository.findUserById(receiverId);
+
+		if (userRelationShipRepository
+			.existsBySenderAndReceiverAndRelationType(sender, receiver, relationType))
+			throw new IllegalStateException("이미 동일한 친구 요청이 존재합니다.");
+
+		UserRelationShip userRelationShip = UserRelationShip.builder()
+			.sender(sender)
+			.receiver(receiver)
+			.relationType(relationType)
+			.build();
+		
+		userRelationShipRepository.save(userRelationShip);
+		return UserInfoDto.fromEntity(receiver);
+	}
 
 	public void cancelRequestFriend(String requestId) {
 		// TODO Auto-generated method stub
