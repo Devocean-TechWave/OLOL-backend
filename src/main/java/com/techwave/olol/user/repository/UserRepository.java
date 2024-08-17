d853c1dce5e7e327f2c1ba0f07bccec396ab9392
hawardShin <joushin@student.42seoul.kr>
2024-08-17 10:59:11 +0900

    Feat: default Function Set Error in repository

--- src/main/java/com/techwave/olol/user/repository/UserRepository.java
+++ src/main/java/com/techwave/olol/user/repository/UserRepository.java
@@ -6,10 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.techwave.olol.user.domain.User;
 
+import jakarta.persistence.EntityNotFoundException;
+
 public interface UserRepository extends JpaRepository<User, String> {
 
 	Optional<User> findByNickname(String nickname);
 
 	Optional<User> findBySnsId(String id);
+
+	default User findUserById(String userId) {
+		return findById(userId)
+			.orElseThrow(() -> new EntityNotFoundException("해당 아이디의 유저를 찾을 수 없습니다: " + userId));
+	}
 }
 
